3c068fef28f1c16b3c3a7b878fa65cf2195320fa
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2023-06-20 18:07:24 -0300

    Upgrade to Version 1

---
+++ js/abrirProducto.js
@@ -0,0 +1,32 @@
+const body = document.body;
+const ventanaEmergente = document.getElementById('ventana');
+const tituloProducto = document.getElementById('tituloProducto');
+const descProducto = document.getElementById('descProducto');
+const precioProducto = document.getElementById('precio');
+const imgProducto = document.getElementById('imgProducto');
+const linkProducto = document.getElementById('linkProducto');
+
+const elementosTarjeta = document.getElementsByClassName('tarjeta');
+for (let i = 0; i < elementosTarjeta.length; i++) {
+  elementosTarjeta[i].addEventListener('click', function() {
+    const titulo = elementosTarjeta[i].querySelector('.titulo').textContent;
+    const descripcion = elementosTarjeta[i].querySelector('.descripcion').textContent;
+    const precio = elementosTarjeta[i].querySelector('.precio').textContent;
+    const imagen = elementosTarjeta[i].querySelector('.img').src;
+    const link = elementosTarjeta[i].querySelector('.linkCatalogo').href;
+
+    tituloProducto.textContent = titulo;
+    descProducto.textContent = descripcion;
+    precioProducto.textContent = precio;
+    imgProducto.src = imagen;
+    linkProducto.href = link;
+
+    ventanaEmergente.style.display = 'block';
+    body.style.overflow = 'hidden';
+  });
+}
+
+document.getElementById('cerrar-ventana').addEventListener('click', function() {
+  ventanaEmergente.style.display = 'none';
+  body.style.overflow = 'auto';
+});
---
+++ js/desplazamientoInicio.js
@@ -0,0 +1,36 @@
+const botonFlotante = document.getElementById('btn_flotante');
+
+botonFlotante.addEventListener('click', () => {
+  scrollToTop(1000);
+});
+
+function scrollToTop(duration) {
+  const startPosition = window.pageYOffset;
+  const distance = -startPosition;
+  const startTime = performance.now();
+
+  function step(timestamp) {
+    const currentTime = timestamp - startTime;
+    const progress = Math.min(currentTime / duration, 1);
+    const ease = easeOutCubic(progress);
+    window.scrollTo(0, startPosition + distance * ease);
+
+    if (currentTime < duration) {
+      window.requestAnimationFrame(step);
+    }
+  }
+
+  window.requestAnimationFrame(step);
+}
+
+function easeOutCubic(t) {
+  return 1 - Math.pow(1 - t, 3);
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    botonFlotante.classList.add('mostrar');
+  } else {
+    botonFlotante.classList.remove('mostrar');
+  }
+});
