bba1428c1736aeb0c9e070dd630725c7eecf25c3
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2023-08-05 18:22:55 -0300

    Upgrade to Version 4.5

--- js/eventos.js
+++ js/eventos.js
@@ -7,8 +7,12 @@ const horaFinalSpan = document.getElementById('horaFinal');
 const organizadorSpan = document.getElementById('organizador');
 
 const proximosEventos = [
-  { dia: 15, mes: 7, horaI: '10', horaF: '14', ubicacion: 'Jovellanos entre Suarez y Olavarria, Barracas,', organizador: 'Ecoferia Barracas' },
-  { dia: 12, mes: 8, horaI: '10', horaF: '14', ubicacion: 'Jovellanos entre Suarez y Olavarria, Barracas,', organizador: 'Ecoferia Barracas' }
+  { dia: 12, mes: 8, horaI: '10', horaF: '14', ubicacion: 'Jovellanos entre Suarez y Olavarria, Barracas,', organizador: 'Ecoferia Barracas' },
+  { dia: 27, mes: 8, horaI: '15', horaF: '18', ubicacion: 'Herrera 799, Barracas,', organizador: 'Polideportivo Don Pepe' },
+  { dia: 9, mes: 9, horaI: '15', horaF: '19', ubicacion: 'Jovellanos entre Suarez y Olavarria, Barracas,', organizador: 'Ecoferia Barracas' },
+  { dia: 7, mes: 10, horaI: '15', horaF: '19', ubicacion: 'Jovellanos entre Suarez y Olavarria, Barracas,', organizador: 'Ecoferia Barracas' },
+  { dia: 16, mes: 12, horaI: '16', horaF: '20', ubicacion: 'Jovellanos entre Suarez y Olavarria, Barracas,', organizador: 'Ecoferia Barracas' },
+  { dia: 23, mes: 12, horaI: '16', horaF: '20', ubicacion: 'Jovellanos entre Suarez y Olavarria, Barracas,', organizador: 'Ecoferia Barracas' }
 ];
 
 const fechaActual = new Date();
