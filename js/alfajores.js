fe32b2557e417531d00e5251624396b0069f8288
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2024-05-27 14:40:24 -0300

    Update alfajores.js

--- js/alfajores.js
+++ js/alfajores.js
@@ -1,6 +1,6 @@
 // "SIN" == Sin Stock | "" == Consultar precio
 
-const preciosDesactualizados = true;
+const preciosDesactualizados = false;
 
 const productosPrecios = [
     {nombre: "Chocolate Intenso", precio: "$1200", desc: "Exquisito alfajor de chocolate negro donde predomina el cacao amargo en la galletita, haciéndolo especial por su intenso sabor a chocolate. Relleno con dulce de leche.<br>Peso neto 70grs."},
