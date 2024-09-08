0eb35f06d9d0c654305f31ef56c0a1f4241ba7f0
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2024-09-08 12:07:25 -0300

    Update productos-precios-eventos.js

--- js/productos-precios-eventos.js
+++ js/productos-precios-eventos.js
@@ -11,7 +11,7 @@ const productosPrecios = [
     {
         nombre: "DDL Sin Azúcar",
         nuevo: true, 
-        precio: "$2000",
+        precio: "$1800",
         desc: "Alfajor endulzado con stevia, relleno con dulce de leche sin azúcar y bañado con chocolate amargo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#sin-azucar",
         linkCatalogo: "https://wa.me/p/123456789/5491120518253"
@@ -19,7 +19,7 @@ const productosPrecios = [
     {
         nombre: "Corazón de Frutos Rojos",
         nuevo: true, 
-        precio: "$1800",
+        precio: "$1600",
         desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con dulce de leche y un dulce de frutos rojos artesanal en el centro. Bañado con chocolate semiamargo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#corazon-de-frutos-rojos",
         linkCatalogo: "https://wa.me/p/123456789/5491120518253"
@@ -35,7 +35,7 @@ const productosPrecios = [
     {
         nombre: "Chocolate Intenso",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor de cacao amargo, relleno con dulce de leche y bañado con chocolate semiamargo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#chocolate-intenso",
         linkCatalogo: "https://wa.me/p/2943239592391020/5491120518253"
@@ -43,7 +43,7 @@ const productosPrecios = [
     {
         nombre: "Chocolate Suave",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con dulce de leche y bañado con chocolate semiamargo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#chocolate-suave",
         linkCatalogo: "https://wa.me/p/3772179199521813/5491120518253"
@@ -51,7 +51,7 @@ const productosPrecios = [
     {
         nombre: "Chocolate Blanco",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con dulce de leche y bañado con chocolate blanco.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#chocolate-blanco",
         linkCatalogo: "https://wa.me/p/2648568928598208/5491120518253"
@@ -59,7 +59,7 @@ const productosPrecios = [
     {
         nombre: "Chocolate con Leche",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con dulce de leche y bañado con chocolate de leche.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#chocolate-con-leche",
         linkCatalogo: "https://wa.me/p/2867341769987819/5491120518253"
@@ -67,7 +67,7 @@ const productosPrecios = [
     {
         nombre: "Maicena",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor de maicena, relleno con dulce de leche y decorado con coco rayado.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#maicena",
         linkCatalogo: "https://wa.me/p/3188162611217945/5491120518253"
@@ -75,7 +75,7 @@ const productosPrecios = [
     {
         nombre: "Maicena Bañada Negro",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor de maicena, relleno con dulce de leche y bañado con chocolate semiamargo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#maicena-banada-negro",
         linkCatalogo: "https://wa.me/p/3522782987738668/5491120518253"
@@ -83,7 +83,7 @@ const productosPrecios = [
     {
         nombre: "Avena",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor de cacao y avena, relleno con dulce de leche y bañado con chocolate semiamargo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#avena",
         linkCatalogo: "https://wa.me/p/3174337869283824/5491120518253"
@@ -91,7 +91,7 @@ const productosPrecios = [
     {
         nombre: "Avena Frutal",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor de cacao y avena, relleno con dulce de membrillo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#avena",
         linkCatalogo: "https://wa.me/p/3584773164969219/5491120518253"
@@ -99,7 +99,7 @@ const productosPrecios = [
     {
         nombre: "Frutos Rojos",
         nuevo: false, 
-        precio: "$1800",
+        precio: "$1600",
         desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con un dulce de frutos rojos artesanal (frutilla, arándanos, mora, frambuesa) y bañado con chocolate semiamargo.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#frutos-rojos",
         linkCatalogo: "https://wa.me/p/5304282312977837/5491120518253"
@@ -107,7 +107,7 @@ const productosPrecios = [
     {
       nombre: "Manzana",
       nuevo: false, 
-      precio: "$1800",
+      precio: "$1600",
       desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con dulce de manzana artesanal y bañado con chocolate semiamargo.<br>Peso neto 70grs.",
       linkPagina: "https://alfajoreslabarraca.com.ar/#manzana",
       linkCatalogo: "https://wa.me/p/3562345450537997/5491120518253"
@@ -115,7 +115,7 @@ const productosPrecios = [
     {
         nombre: "Membrillo Negro",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con dulce de membrillo y bañado con chocolate blanco.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#membrillo-negro",
         linkCatalogo: "https://wa.me/p/5071317579609956/5491120518253"
@@ -123,7 +123,7 @@ const productosPrecios = [
     {
         nombre: "Merengue con Dulce de Leche",
         nuevo: false, 
-        precio: "$1600",
+        precio: "$1400",
         desc: "Alfajor con una delicada cantidad de cacao amargo, relleno con dulce de leche y bañado con merengue italiano.<br>Peso neto 70grs.",
         linkPagina: "https://alfajoreslabarraca.com.ar/#merengue-con-dulce-de-leche",
         linkCatalogo: "https://wa.me/p/4661400207319943/5491120518253"
