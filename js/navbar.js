c0452235efd876a9eec69754e0e9ed63b1de4f7d
FranRazzitte <98547220+FranRazzitte@users.noreply.github.com>
2025-03-01 02:18:56 -0300

    Upgrade to Version 11.1.1

--- js/navbar.js
+++ js/navbar.js
@@ -18,7 +18,7 @@ navbarContent.innerHTML = `
             <div class="col-md-4 justify-content-left justify-content-md-start mb-md-0 navDiv2" id="navDiv2">
                 <form class="input-group w-100 my-auto d-none d-sm-flex" id="formSearch">
                     <input
-                        data-lang="lb-placeholder-navbar-section:search"
+                        lang="lb-placeholder-navbar-section:search"
                         autocomplete="off"
                         id="buscar"
                         class="form-control rounded"
@@ -45,9 +45,9 @@ navbarContent.innerHTML = `
                             <svg id="auto-icon" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" style="fill: var(--clr-general);"><path d="M440-120q-134 0-227-93t-93-227q0-134 93-227t227-93q5 0 10 .5t10 .5q-29 32-44.5 73T400-600q0 100 70 170t170 70q31 0 60.5-7.5T756-390q-18 118-108 194t-208 76Zm112-400 128-360h80l128 360h-76l-28-80H656l-28 80h-76Zm122-134h92l-46-146-46 146Z"/></svg>
                         </button>
                         <ul class="dropdown-menu" id="dropdownMenu">
-                            <li><button data-lang="lb-btn-navbar-section:light-mode" class="dropdown-item" id="light-mode">Modo Claro</button></li>
-                            <li><button data-lang="lb-btn-navbar-section:night-mode" class="dropdown-item" id="dark-mode">Modo Oscuro</button></li>
-                            <li><button data-lang="lb-btn-navbar-section:default" class="dropdown-item selected" id="auto-mode">Igualar al Sistema</button></li>
+                            <li><button lang="lb-btn-navbar-section:light-mode" class="dropdown-item" id="light-mode">Modo Claro</button></li>
+                            <li><button lang="lb-btn-navbar-section:night-mode" class="dropdown-item" id="dark-mode">Modo Oscuro</button></li>
+                            <li><button lang="lb-btn-navbar-section:default" class="dropdown-item selected" id="auto-mode">Igualar al Sistema</button></li>
                         </ul>
                     </div>
                 </div>
